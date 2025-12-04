db4613fdd137cc945db40b9560d4eac2d9e30e36
TWITCHGAMER34 <nazoma05@gmail.com>
2025-12-04 16:38:55 +0100

    Fixed Auth

--- index.js
+++ index.js
@@ -2,16 +2,18 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const knexLib = require('knex');
 const createSession = require('./middlewares/session');
-const authRoutes = require('./routes/auth');
+const authRoutesFactory = require('./routes/auth');
 const knexConfig = require('./knexfile.js');
+const {hashPassword} = require("./utils/password");
 
 const env = process.env.NODE_ENV || 'development';
 const knex = knexLib(knexConfig[env] || knexConfig); // create a Knex instance from the config
 
+const authRoutes = authRoutesFactory(knex);
 const menuRoutes = require('./routes/menu')(knex);
+const orderRoutes = require('./routes/orders')(knex);
 
 const app = express();
 app.use(express.json());
@@ -26,9 +28,13 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // provide a session secret so the middleware can create sessions
-app.use(createSession({ secret: process.env.SESSION_SECRET || 'change-me' }));
+app.use(createSession({secret: process.env.SESSION_SECRET || 'Keyboard Cat'}));
+
 
 app.use('/auth', authRoutes);
 app.use('/menu', menuRoutes);
+app.use('/orders', orderRoutes);
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+})
